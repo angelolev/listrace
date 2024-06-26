a5f41716e603ee7d63b88d9a244169b602e00a80
Angelo Leva <angelokta7@gmail.com>
2024-06-26 16:32:59 -0500

    chore: get domain

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -3,13 +3,13 @@ import { IEntry } from "@/types";
 import { getData } from "@/utils";
 
 export default async function Posts() {
-  const data = await getData("posts");
+  // const data = await getData("posts");
 
   return (
     <div className="posts grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 pt-[80px]">
-      {data.map((item: IEntry) => (
+      {/* {data.map((item: IEntry) => (
         <Entry key={item.id} {...item} />
-      ))}
+      ))} */}
     </div>
   );
 }
