1bab4d9125e539deac40999894542bec9d99d0c7
Angelo Leva <angelokta7@gmail.com>
2024-06-26 12:36:14 -0500

    chore: updated endpoint logs

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -1,9 +1,13 @@
 export default async function getData(path: string) {
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/${path}`);
+    console.log(`Fetching data from: ${process.env.NEXT_PUBLIC_API_URL}`);
 
     if (!res.ok) {
-      throw new Error("Failed to fetch data");
+      const errorText = await res.text();
+      console.error("Fetch error:", res.status, res.statusText, errorText);
+      throw new Error(`Failed to fetch data: ${res.statusText}`);
+      // throw new Error("Failed to fetch data");
     }
 
     return await res.json();
